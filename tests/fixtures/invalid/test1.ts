ab98204073aa184a034fa55973358ab6e7d69179
str <str_ie@yahoo.co.uk>
2021-03-02 20:38:20 +0100

    chore: fix lint

--- tests/fixtures/invalid/test1.ts
+++ tests/fixtures/invalid/test1.ts
@@ -1,6 +1,7 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 class MyClass {
   doSomethingResolved() {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve, _reject) => {
       return void resolve("error!");
     });
   }
